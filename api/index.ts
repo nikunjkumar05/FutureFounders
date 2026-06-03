bda0a5c9999e9c049d067c944d293b59a7d9626d
nikunjkumar05 <kumarnikunj1207@gmail.com>
2026-06-03 23:59:49 +0530

    build

--- api/index.ts
+++ api/index.ts
@@ -1,3 +1,11 @@
-import { app } from "../apps/api/src/app.js";
+import type { Request, Response } from "express";
 
-export default app;
+let app: any;
+
+export default async function handler(req: Request, res: Response) {
+  if (!app) {
+    const mod = await import("../apps/api/src/app.js");
+    app = mod.app;
+  }
+  return app(req, res);
+}
